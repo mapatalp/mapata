0269571f7f4dc2a40de4946ecf0926506580d589
Francisco-G-Montero <gabixp45@gmail.com>
2022-11-17 10:28:44 -0300

    fixing profile image

--- src/components/Shapes/UserProfilePic.js
+++ src/components/Shapes/UserProfilePic.js
@@ -6,7 +6,6 @@ const styles = StyleSheet.create({
       flex: 1,
       justifyContent: "center",
       alignItems: "center",
-      backgroundColor: "#e5e5e5",
     },
     headerText: {
       fontSize: 20,
@@ -20,7 +19,12 @@ const styles = StyleSheet.create({
       borderRadius: 150 / 2,
       backgroundColor: '#B2B2B2',
       borderColor: "#D9D9D9",
-      borderWidth: 10
+      borderWidth: 10,
+      alignContent: "center",
+      alignItems: "center",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center"
     }
   
   });
@@ -32,7 +36,8 @@ const UserProfilePic = () => {
     return (
         <View style={styles.container}>
           <View style={styles.CircleShape}>
-            <Image resizeMode="cover" source={{uri: "https://cdn-icons-png.flaticon.com/512/616/616408.png"}} ></Image>
+            <Image source={{uri: "https://cdn-icons-png.flaticon.com/512/616/616408.png"}}
+              style={{width: "70%", height: "70%" }}/>
           </View>
         </View>
       );
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { View, Text } from "react-native"; //por que verga tengo que importarlo de aca si en el login screen lo hace desde components
-import { UserProfilePic } from "../components";
+import { View, Text, Image } from "react-native"; //por que verga tengo que importarlo de aca si en el login screen lo hace desde components
+import { UserProfilePic, Row } from "../components";
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import AsdScreen from "./AsdScreen";
 const Tab = createMaterialTopTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator style={{marginTop: 115}}>
       <Tab.Screen name="Home" component={AsdScreen} />
       <Tab.Screen name="Settings" component={AsdScreen} />
     </Tab.Navigator>
@@ -17,8 +17,10 @@ function MyTabs() {
 const ProfileScreen = () => {
   return (
     <View style={{ height: "100%", width: "100%" }}>
-      <UserProfilePic/>
-      <Text style={{ alignSelf: "center" }}>Login screen</Text>
+      <Row additionalStyles={{ marginTop: 25 }}>
+        <UserProfilePic/>
+      </Row>
+      <Text style={{ alignSelf: "center", marginTop: 15, fontWeight: "bold", fontSize: 20}}>Mr. Buny</Text>
       <MyTabs/>
     </View>
   );
