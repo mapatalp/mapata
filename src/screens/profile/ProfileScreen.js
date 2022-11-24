8e0a297b6ec82949edec34836fc7c9f0e7d8a666
ramiro <rami992009@gmail.com>
2022-11-24 16:23:19 -0300

    avances

--- src/screens/profile/ProfileScreen.js
+++ src/screens/profile/ProfileScreen.js
@@ -1,21 +1,26 @@
-import React, { useEffect } from "react";
-import { View } from "react-native"; //por que verga tengo que importarlo de aca si en el login screen lo hace desde components
+import React, { useMemo } from "react";
+import { View, ScrollView } from "react-native"; //por que verga tengo que importarlo de aca si en el login screen lo hace desde components
 import { UserProfilePic, Row, SocialButton, Text } from "../../components";
 import { ProfileTabsScreen } from "./ProfileTabsScreen";
 import { getMockedProfile } from "../../utils/ProfileHelper";
 import DescriptionCard from "../../components/Cards/DescriptionCard";
 import { Carousel } from "../../components";
-import { ScrollView } from "react-native-gesture-handler";
 import { store } from "../../redux";
+import { useTheme } from "react-native-paper";
 
 const ProfileScreen = () => {
   let isRefugio = false;
   let isSelf = true;
   let profile = getMockedProfile();
-  const { user } = store;
+  const { user } = store.getState();
+  const { colors } = useTheme();
+  console.log(
+    "🚀 ~ file: ProfileScreen.js ~ line 16 ~ ProfileScreen ~ user",
+    user
+  );
 
-  if (!user) return null;
-  
+  const name = useMemo(() => user?.data?.usename, [user]);
+
   return (
     <View style={{ height: "100%", width: "100%" }}>
       <ScrollView>
@@ -26,9 +31,10 @@ const ProfileScreen = () => {
             marginTop: 15,
             fontWeight: "bold",
             fontSize: 20,
+            color: colors.black,
           }}
         >
-          {user.data.usename}
+          {name}
         </Text>
         {isRefugio && <DescriptionCard text={profile.description} />}
         <Row
